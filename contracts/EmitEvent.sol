b6e52a3fb341fc751622c4720bd9646347323857
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-02-01 21:53:13 +0000

    Transaction reverted: function call to a non-contract account

---
+++ contracts/EmitEvent.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+interface WinnerContract {
+    function attempt() external;
+}
+
+contract EmitEvent {
+    function sendAttempt(address _contract) public {
+        return WinnerContract(_contract).attempt();
+    }
+
+}
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,20 +3,16 @@ const hre = require("hardhat");
 async function main() {
 	const EmitEvent = await hre.ethers.getContractFactory("EmitEvent");
 	const emitEvent = await EmitEvent.deploy();
-
-	const addressWinnerContract = "";
+	const addressWinnerContract = "0xcF469d3BEB3Fc24cEe979eFf83BE33ed50988502";
 
 	await emitEvent.deployed();
 
 	// Call the "attempt" method of Winner Contract through the interface inside our Contract
 	const result = await emitEvent.sendAttempt(addressWinnerContract);
-	console.log(result);
 
 	console.log(`Contract deployed to ${emitEvent.address}`);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
 	console.error(error);
 	process.exitCode = 1;
