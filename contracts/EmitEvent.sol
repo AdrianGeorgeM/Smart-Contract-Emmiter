87575c91dd16819f4b9c3ed4637ba983b35d57d6
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-02-02 20:33:29 +0000

    Update the contract and send the transaction to network
    Send the transaction to the network and wait for it to be mined (included in a block)Deploy the contract
    	// await emitWinner.triggerWinningAttempt(emmitterContractAddress);

--- contracts/EmitEvent.sol
+++ contracts/EmitEvent.sol
@@ -1,16 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
-
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
-
 interface WinnerContract {
     function attempt() external;
 }
 
 contract EmitEvent {
+    // This is the event that will be emitted when the function is called from the other contract 
     function sendAttempt(address _contract) public {
-        return WinnerContract(_contract).attempt();
+        WinnerContract(_contract).attempt();
     }
-
 }
+
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,15 +1,19 @@
 const hre = require("hardhat");
 
 async function main() {
-	const EmitEvent = await hre.ethers.getContractFactory("EmitEvent");
-	const emitEvent = await EmitEvent.deploy();
-	const addressWinnerContract = "0xcF469d3BEB3Fc24cEe979eFf83BE33ed50988502";
+	const EmitEvent = await hre.ethers.getContractFactory("EmitEvent"); // Contract name
+	const emitEvent = await EmitEvent.deploy(); // Deploy the contract
+	// https://goerli.etherscan.io/address/0xcF469d3BEB3Fc24cEe979eFf83BE33ed50988502#events
 
-	await emitEvent.deployed();
+	const addressWinnerContract = "0xcf469d3beb3fc24cee979eff83be33ed50988502"; // Winner Contract address
+	await emitEvent.deployed(); // Deploy the contract
+	// await emitWinner.triggerWinningAttempt(emmitterContractAddress);
 
 	// Call the "attempt" method of Winner Contract through the interface inside our Contract
-	const result = await emitEvent.sendAttempt(addressWinnerContract);
+	await emitEvent.sendAttempt(addressWinnerContract);
+	const result = await emitEvent.sendAttempt(addressWinnerContract); // Send the transaction to the network and wait for it to be mined (included in a block)
 
+	console.log("Transaction: ", result);
 	console.log(`Contract deployed to ${emitEvent.address}`);
 }
 
